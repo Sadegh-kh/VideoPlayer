89bbcde0fb6631861755fa7d4ef933e62b2231ea
Sadegh <Sadeghkhoshbayan1@gmail.com>
2022-07-27 21:05:02 +0430

    init Exo player

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.videoplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
 import com.example.videoplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 
-const val URL =
+const val URI =
     "https://hw20.asset.aparat.com/aparat-video/ec18dea76e6a4fab8f3eceb4e1a9dce711376561-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjU5ODQzYzI5ZjZiNzJmY2NlZjc4NWY0MDRlYmE2YTA4IiwiZXhwIjoxNjU4OTQ3MzE5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.tHGCdCmu0SUaZNntZ-ETxTmcTbgP9ZNrRWsgraNJcWM"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var exoPlayer: ExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+/*  Video View
         binding.videoView.setVideoPath(URL)
 
         //when Prepared video  , it'll start
@@ -24,7 +28,8 @@ class MainActivity : AppCompatActivity() {
             mediaController.setMediaPlayer(binding.videoView)
             binding.videoView.setMediaController(mediaController)
             mediaController.setAnchorView(binding.videoView)
-        }
+        }*/
+
 /* More attribute for Video View
         binding.videoView.duration
         binding.videoView.currentPosition
@@ -37,10 +42,19 @@ class MainActivity : AppCompatActivity() {
             true
         }*/
 
+        exoPlayer = ExoPlayer.Builder(this).build()
+        val mediaItem = MediaItem.fromUri(URI)
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
+        exoPlayer.playWhenReady=true
+        exoPlayer.play()
+
+        binding.PlayerVideo.player=exoPlayer
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        binding.videoView.stopPlayback()
+//        binding.videoView.stopPlayback()
+        exoPlayer.release()
     }
 }
